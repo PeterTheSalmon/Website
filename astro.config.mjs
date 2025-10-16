fbf7b6cca4dccc124c925f2bdbf55998bdcb8903
PeterTheSalmon <pdfsalmon@gmail.com>
2025-10-16 11:22:38 -0700

    Conversion to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
+import tailwind from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpine()],
+  vite: {
+    plugins: [tailwind()],
+  },
+});
